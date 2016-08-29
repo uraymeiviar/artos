29f1f846483c91ec67ed0d73a2620e8bfeb7fe8d
uraymeiviar <uraymeiviar@gmail.com>
2016-08-30 00:15:20 +0700

    updated initramfs build script

--- build/initramfs/build.sh
+++ build/initramfs/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 BUSYBOX_VER="1.25.0"
 BUSYBOX_DOWNLOAD_URL="https://busybox.net/downloads/busybox-$BUSYBOX_VER.tar.bz2"
+mkdir -p root
+tar -Jxvf ./rootfs.tar.xz
+cp -rf ./rootfs/* ./root/
+cp -rf ./livekit/* ./root/
 wget "$BUSYBOX_DOWNLOAD_URL"
 tar -jxvf "busybox-$BUSYBOX_VER.tar.bz2"
 cd "busybox-$BUSYBOX_VER"
@@ -8,9 +12,6 @@ make clean
 cp ../busybox.config ./.config
 LDFLAGS=-static CFLAGS="-O2 -march=x86-64" make
 make install
-mkdir -p ../root
-cp -rf ../rootfs/* ../root/
-cp -rf ../livekit/* ../root/
 cp -rf ./_install/* ../root/
 ../../bin/dir2initrfs ./root ./initrfs.img
 
--- build/initramfs/clean.sh
+++ build/initramfs/clean.sh
@@ -2,4 +2,5 @@
 rm -rf ./*.tar.bz2
 rm -rf ./busybox-*
 rm -rf ./root
+rm -rf ./rootfs
 rm ./initrfs.img
