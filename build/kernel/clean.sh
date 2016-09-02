5c7ed90dc4a0a2913c528ddf9093c0c28c6ad750
uraymeiviar <uraymeiviar@gmail.com>
2016-09-03 04:03:25 +0700

    updated kernel config and build script

---
+++ build/kernel/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf ./downloaded
+rm -rf ./kernel-headers
+rm -rf ./kernel-modules
+rm -rf ./kernel-src*
+rm -f ./vmlinuz
+rm -rf ./aufs*
+rm -f ./*.sb
--- build/kernel/rebuild.sh
+++ build/kernel/rebuild.sh
@@ -1,35 +1,7 @@
-#!/bin/sh
+#/bin/sh
 KERNEL_VER="4.8-rc4"
-#download kernel source
-mkdir -p ./downloaded
-wget "https://cdn.kernel.org/pub/linux/kernel/v4.x/testing/linux-$KERNEL_VER.tar.xz" -P ./downloaded/
 
-#extract kernel source
-mkdir -p ./kernel-src
-tar -Jxvf "./downloaded/linux-$KERNEL_VER.tar.xz" -C ./kernel-src
-
-#checkout aufs patch
-git clone https://github.com/sfjro/aufs4-standalone.git
-cd aufs4-standalone
-git checkout origin/aufs4.x-rcN
-
-#copy patch files
-cp -rf ./Documentation ./fs "./../kernel-src/linux-$KERNEL_VER/"
-cp ./include/uapi/linux/aufs_type.h "./../kernel-src/linux-$KERNEL_VER/include/uapi/linux/"
-cd ..
-
-#checkout aufs-util
-git clone git://git.code.sf.net/p/aufs/aufs-util
-cd aufs-util
-git checkout origin/aufs4.x-rcN
-cd ..
-
-#patch the kernels
 cd "./kernel-src/linux-$KERNEL_VER"
-patch -p1 < ../../aufs4-standalone/aufs4-kbuild.patch
-patch -p1 < ../../aufs4-standalone/aufs4-base.patch
-patch -p1 < ../../aufs4-standalone/aufs4-mmap.patch
-patch -p1 < ../../aufs4-standalone/aufs4-standalone.patch
 
 #build the kernel
 make mrproper
