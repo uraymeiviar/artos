788d6fbec34d937c6d59c711f07d0acc5e7ae4a8
uray meiviar <uraymeiviar@gmail.com>
2016-11-23 00:04:50 +0700

    fixed gcc pass 1 build script

--- build/crossbuild/build-gcc-pass1.sh
+++ build/crossbuild/build-gcc-pass1.sh
@@ -52,8 +52,25 @@ if [ ! -d ./extracts/$PKG ]; then
 	ln -s ../mpfr mpfr
 	ln -s ../mpc mpc
 	ln -s ../gmp gmp
-        cd ../..
+        cd ../../
 fi
+
+cd ./extracts/$PKG
+
+for file in \
+ $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
+do
+  cp -uv $file{,.orig}
+  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+      -e 's@/usr@/tools@g' $file.orig > $file
+  echo '
+#undef STANDARD_STARTFILE_PREFIX_1
+#undef STANDARD_STARTFILE_PREFIX_2
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+  touch $file.orig
+done
+cd ../../
 
 cd ./builds
 mkdir -p ./$PKG_BUILD
