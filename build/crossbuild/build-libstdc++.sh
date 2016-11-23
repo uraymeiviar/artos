b9b89127a87b4720c3ddf0e5fd3f294204083aa5
uray meiviar <uraymeiviar@gmail.com>
2016-11-23 08:20:57 +0700

    added build script for libstdc++

---
+++ build/crossbuild/build-libstdc++.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+PKG="gcc"
+PKG_BUILD="libstdc++"
+source ./config.sh
+
+if [ ! -f ./sources/$PKG.txz ]; then
+	wget http://files.etherlink.co/mirror/$PKG/$PKG-latest.txz -O ./sources/$PKG.txz
+fi
+
+if [ ! -d ./extracts/$PKG ]; then
+	dirname=`tar -tf ./sources/$PKG.txz | head -1 | cut -f1 -d"/"`
+	cd ./extracts
+	tar -xf ../sources/$PKG.txz
+	mv $dirname $PKG
+	cd ..
+fi
+
+cd ./builds
+mkdir -p ./$PKG_BUILD
+cd ./$PKG_BUILD
+
+../../extracts/$PKG/libstdc++-v3/configure --prefix=/tools            \
+    --host=$LFS_TGT                 \
+    --disable-multilib              \
+    --disable-nls                   \
+    --disable-libstdcxx-threads     \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/6.2.0
+
+echo "compiling $PKG_BUILD"
+make
+
+echo "installing $PKG_BUILD"
+make install
